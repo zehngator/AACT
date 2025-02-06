2ef70b0445328d2881ca74285e143292fa1e6160
matt <m267146@usna.edu>
2025-02-05 22:47:20 -0500

    got ollama to give me a command

--- termcom/different_visible_term.py
+++ termcom/different_visible_term.py
@@ -4,16 +4,50 @@ import time
 
 def query_ollama(prompt):
     """ Sends a prompt to Ollama and gets a response """
-    response = ollama.chat(model='dolphin-mistral', messages=[{"role": "user", "content": prompt}])
+    response = ollama.chat(model='dolphin-mistral', 
+                           messages=[{"role": "system", "content": prompt}])
+    with open('last_response.txt', "w") as f:
+        f.write(response['message']['content'])
     return response['message']['content']
 
+def tool_ollama(prompt):
+    """ Sends a prompt to Ollama and gets a response """
+    response = ollama.chat(model='llama3.1:8b', 
+                           messages=[{"role": "user", "content": prompt}],
+                           tools=[{
+                               'type': "function",
+                               'function': {
+                                   'name': 'extract_code',
+                                   'description': 'Extracts code for use on the command line',
+                                   'parameters' : {
+                                       'type': 'object',
+                                       'properties': {
+                                           "code_type" : {
+                                               "type": "string",
+                                               "description": "The type of code that is getting extract"
+                                           },
+                                           "code" :{
+                                               "type": "string",
+                                               "description": "The code that is getting extract"
+                                           }
+                                       },
+                                        'required': ['code_type', 'code']
+                                   }
+                               }
+                           }])
+    print(response['message']['tool_calls'])
+    with open('last_response.txt', "w") as f:
+        f.write(response['message']['content'])
+    return response['message']['tool_calls']
+
 def extract_code(mess):
-    start = mess.find("'''")
-    codetype = mess[:start].split()[-1]
+    start = mess.find("```")
+    print(start)
+    #codetype = mess[start+3:].split()[0]
     code = mess[start+3:]
-    end = code.find("'''")
-    code = code[:end]
-    return [codetype,code]
+    end = code.find("```")
+    code = code[:end-1]
+    return code
 
 
 def run_terminal():
@@ -26,33 +60,41 @@ def run_terminal():
 
     # Initial problem description
     problem_description = "Solve a ctf at the ip 10.10.10.245"
-    next_command = query_ollama(f"{problem_description} Suggest the first command.")
+    #next_command = query_ollama(f"{problem_description} Suggest the first command.")
+    next_command = tool_ollama(f"{problem_description} Suggest the first command.")
 
     count = 0
-    def run_bash(code):
-        # Send command to terminal
-        term.stdin.write(next_command + "\n")
-        term.stdin.flush()
+    # def run_bash(code):
+    #     # Send command to terminal
+    #     term.stdin.write(next_command + "\n")
+    #     term.stdin.flush()
     
-    def run_python(code):
-        name = str(count)+'.py'
-        with open(name,"w") as f:
-            f.write(code)
-        print(f"[+] created python code under {name}")
-        count += 1
+    # def run_python(code):
+    #     name = str(count)+'.py'
+    #     with open(name,"w") as f:
+    #         f.write(code)
+    #     print(f"[+] created python code under {name}")
+    #     count += 1
 
 
     while True:
-        command_set = extract_code(next_command)
+        #command_set = extract_code(next_command)
+        print(type(next_command))
+        #print(next_command[0]['name'])
+        print(next_command[0].function['arguments']['code'])
+        
+        command_set = next_command[0]['function']['arguments']['code']
         print(f"[+] Ollama suggests: {command_set}")
 
-        if command_set[0] == 'bash':
-            run_bash(command_set[1])
+        # if command_set[0] == 'bash':
+        #     run_bash(command_set[1])
         
-        if command_set[0] == 'python':
-            run_python(command_set[1])
-            command_set[1] += f"\n This code was saved under {count}.py"
+        # if command_set[0] == 'python':
+        #     run_python(command_set[1])
+        #     command_set[1] += f"\n This code was saved under {count}.py"
         
+        term.stdin.write(command_set + "\n")
+        term.stdin.flush()
         # Give the command time to execute
         time.sleep(2)
 
